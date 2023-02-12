6c09db92242a792fc0085332923537238dc50bee
Dan Baker <daniel.c.baker@gmail.com>
2023-02-11 18:01:38 -0800

    header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,14 @@ export default function Header() {
     <div className='header'>
       <div>danbaker</div>
       <div className='socials'>
-        <img src={GitHub} alt='github' />
-        <img src={LinkedIn} alt='linkedin' />
-        <img src={Twitter} alt='twitter' />
+        <a href='https://github.com/legshampoo' target='_blank'>
+          <img src={GitHub} alt='github' />
+        </a>
+        <a href='https://www.linkedin.com/in/dan-baker-4703a189/' target='_blank'>
+          <img src={LinkedIn} alt='linkedin' />
+        </a>
+
+        {/* <img src={Twitter} alt='twitter' /> */}
       </div>
     </div>
   )
