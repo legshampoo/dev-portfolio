fee6cc52a611752f27b161ec277f5462b2e81447
Dan Baker <daniel.c.baker@gmail.com>
2023-02-14 13:41:21 -0800

    adding banana project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 
-import Thumb_1 from '../static/img/thumb/thumbnail-project-1-large.webp'
-import Thumb_2 from '../static/img/thumb/thumbnail-project-2-large.webp'
-import Thumb_3 from '../static/img/thumb/thumbnail-project-3-large.webp'
-import Thumb_4 from '../static/img/thumb/thumbnail-project-4-large.webp'
+import coinTracker from '../static/img/thumb/thumbnail-project-1-large.webp'
+import dnbkr from '../static/img/thumb/thumbnail-project-2-large.webp'
+import devPortfolio from '../static/img/thumb/thumbnail-project-3-large.webp'
+import banana from '../static/img/thumb/thumbnail-project-4-large.webp'
 import Thumb_5 from '../static/img/thumb/thumbnail-project-5-large.webp'
 import Thumb_6 from '../static/img/thumb/thumbnail-project-6-large.webp'
 
@@ -16,24 +16,32 @@ export default function Projects() {
         <ProjectCard 
           name={'CRYPTO TOKEN TRACKER'}
           skills={['REACT', 'HTML', 'CSS']}
-          thumb={Thumb_1}
+          thumb={coinTracker}
           id={1}
           url={'https://coin-tracker-demo.vercel.app'}
           git={'https://github.com/legshampoo/coin-tracker-demo'}
         />
         <ProjectCard 
+          name={'NFT LANDING PAGE'}
+          skills={['REACT', 'REACT FIBER', 'THREEJS']}
+          thumb={banana}
+          id={2}
+          url={'https://banana-landing-page.vercel.app/'}
+          git={'https://github.com/legshampoo/dev-portfolio'}
+        />
+        <ProjectCard 
           name={'IMMERSIVE ART PORTFOLIO'}
           skills={['NODE', 'HTML', 'CSS']}
-          thumb={Thumb_2}
-          id={2}
+          thumb={dnbkr}
+          id={3}
           url={'https://dnbkr.com'}
           git={'https://github.com/legshampoo/update.dnbkr.com'}
         />
         <ProjectCard 
-          name={'DEVELOPER PORTFOLIO (THIS SITE!)'}
+          name={'THIS SITE!'}
           skills={['REACT', 'HTML', 'CSS', 'JAVASCRIPT', 'NEXTJS']}
-          thumb={Thumb_3}
-          id={3}
+          thumb={devPortfolio}
+          id={4}
           url={'https://danbaker.codes'}
           git={'https://github.com/legshampoo/dev-portfolio'}
         />
